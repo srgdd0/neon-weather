a592b8479bd15115afbe1817d696208f884a8954
Serhii Didenko <srg.dd0@gmail.com>
2024-03-11 08:16:30 +0200

    feat: Implement weather data fetching and display
    Add functionality to fetch current weather data from the OpenWeatherMap API for Kokkola and dynamically update the UI with the retrieved information. Display key weather metrics such as temperature, description, wind speed, humidity, and sunrise/sunset times.
    
    This enhancement significantly improves the user experience by providing real-time, location-specific weather data. The UI has been updated with dedicated sections for displaying the fetched information in a visually appealing and intuitive manner.
    
    Resolves #12

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-import "./style.css";
+const name = document.querySelector("#name");
+const temperature = document.querySelector(".temperature");
+const temperatureMax = document.querySelector("#max");
+const temperatureMin = document.querySelector("#min");
+const feelsLike = document.querySelector("#feels_like");
+const weatherDescription = document.querySelector(".weather-description");
+const windSpeed = document.querySelector("#wind-speed");
+const humidity = document.querySelector("#Humidity");
+const sunrise = document.querySelector("#sunrise");
+const sunset = document.querySelector("#sunset");
+
+const data = await fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=kokkola&units=metric&&appid=89b796e5dba23e291bbea89229545a11`
+)
+  .then((res) => res.json())
+  .catch((err) => console.log(`error: ${err}`));
+
+console.log(data);
+name.innerHTML = data.name;
+temperature.innerHTML = Math.round(data.main.temp) + "°C";
+feelsLike.innerHTML += Math.round(data.main.feels_like) + "°C";
+weatherDescription.innerHTML = data.weather[0]["description"];
+windSpeed.innerHTML += Math.round(data.wind.speed) + "m/s";
+humidity.innerHTML += data.main.humidity + "%";
+temperatureMax.innerHTML += Math.round(data.main.temp_max) + "°C";
+temperatureMin.innerHTML += Math.round(data.main.temp_min) + "°C";
+sunrise.innerHTML += new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+sunset.innerHTML += new Date(data.sys.sunset * 1000).toLocaleTimeString();
